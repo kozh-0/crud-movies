952ea3e68cf57c3d04f483eafe34369771e13527
Dmitry Kozhevnikov <kdaufa@mail.ru>
2022-11-21 05:15:39 +0500

    registration & login

--- index.ts
+++ index.ts
@@ -10,51 +10,73 @@ const db = mysql.createPool({
     password: 'password',
     database: 'crud server'
 });
-const table = 'movie_reviews';
 const port = 3001;
-
+// Вроде как безопасный протокол передачи данных
 app.use(cors());
+// авто парс json
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/get', (req, res) => {
-    const sqlGet = `SELECT * FROM ${table}`;
-    db.query(sqlGet, (err, result) => res.send(result));
-});
 
-app.post('/api/insert', (req, res) => {
-    const { movieName, movieReview } = req.body;
-    const sqlInsert = `INSERT INTO ${table} (movieName, movieReview) VALUES (?, ?)`;
+// reviews
+{
+    const table = 'movie_reviews';
 
-    db.query(sqlInsert, [movieName, movieReview], (err, result) => console.log(result));
-});
-
-app.delete('/api/delete/:movieName', (req, res) => {
-    const { movieName } = req.params;
-    // ? мы ставим, чтобы в db.query передать этот параметр
-    const sqlDelete = `DELETE FROM ${table} WHERE movieName = ?`;
-    db.query(sqlDelete, movieName, (err, result) => {
-        if (err) console.error(err);
-    })
-    res.send('delete')
-});
-
-app.put('/api/update', (req, res) => {
-    const { movieName, movieReview } = req.body;
-    const sqlUpdate = `UPDATE ${table} SET movieReview = ? WHERE movieName = ?`;
-
-    db.query(sqlUpdate, [movieReview, movieName], (err, res) => {
-        if (err) console.error(err);
+    app.get('/reviews/get', (req, res) => {
+        const sqlGet = `SELECT * FROM ${table}`;
+        db.query(sqlGet, (err, result) => res.send(result));
     });
-});
 
+    app.post('/reviews/insert', (req, res) => {
+        const { movieName, movieReview } = req.body;
+        const sqlInsert = `INSERT INTO ${table} (movieName, movieReview) VALUES (?, ?)`;
+
+        db.query(sqlInsert, [movieName, movieReview], (err, result) => console.log(result));
+    });
+
+    app.delete('/reviews/delete/:movieName', (req, res) => {
+        const { movieName } = req.params;
+        // ? мы ставим, чтобы в db.query передать этот параметр
+        const sqlDelete = `DELETE FROM ${table} WHERE movieName = ?`;
+        db.query(sqlDelete, movieName, (err, result) => {
+            if (err) console.error(err);
+        })
+        res.send('delete')
+    });
+
+    app.put('/reviews/update', (req, res) => {
+        const { movieName, movieReview } = req.body;
+        const sqlUpdate = `UPDATE ${table} SET movieReview = ? WHERE movieName = ?`;
+
+        db.query(sqlUpdate, [movieReview, movieName], (err, res) => {
+            if (err) console.error(err);
+        });
+    });
+}
+
+// register
+{
+    const table = 'users';
+    app.post('/register', (req, res) => {
+        const { username, email, password } = req.body;
+
+        const sqlReg = `INSERT INTO ${table} (username, email, password) VALUES (?,?,?)`;
+        db.query(sqlReg, [username, email, password], (err, result) => console.log(err));
+    });
+
+    app.post('/login', (req, res) => {
+        const { email, password } = req.body;
+
+        const sqlLogin = `SELECT * FROM ${table} WHERE email = ? AND password = ?`;
+        db.query(sqlLogin, [email, password], (err, result) => {
+            console.log(email, password);
+            
+            if (err) res.send({ err });
+
+            result.length > 0 ? res.send(result) : res.send({ message: 'There is no such user!' });
+        });
+    });
+}
 
 
 app.listen(port, () => console.log(`running on port ${port}`));
-
-// app.get('/api', (req: Request, res: Response) => {
-//     const sqlInsert = `INSERT INTO ${table} (movieName, movieReview) VALUES ('Inception', 'Good movie');`
-//     db.query(sqlInsert, (err, result) => {
-//         res.send({kek: 228});
-//     });
-// });
