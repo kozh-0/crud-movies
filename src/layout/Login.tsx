77dcbbf041f42270080589beb09ac876db7271a1
Dmitry Kozhevnikov <kdaufa@mail.ru>
2022-11-21 05:13:52 +0500

    reg & login validation

--- src/layout/Login.tsx
+++ src/layout/Login.tsx
@@ -1,25 +1,42 @@
+import axios from "axios";
 import { useState } from "react"
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [loginStatus, setLoginStatus] = useState('');
 
-    return (
+    const auth = () => {
+        if (!email || !password) return;
+        axios.post('http://localhost:3001/login', {
+            email: email.trim(),
+            password: password.trim()
+        })
+        .then(({data}) => {
+            console.log(data);
+            
+            Array.isArray(data) ? setLoginStatus('Authed') : setLoginStatus('НЕТ')
+        });
+        setEmail(''); setPassword('');
+    }
+
+    return <>
         <div className="input_div">
             <input
                 autoFocus
                 type="email"
-                placeholder="email"
+                placeholder="E-mail"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                />
+            />
             <input
-                placeholder="Your review"
                 type="password"
+                placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             />
-            <button>Login</button>
+            <button onClick={auth}>Login</button>
         </div>
-    )
+        <h1>{loginStatus}</h1>
+    </>;
 }
--- src/layout/Register.tsx
+++ src/layout/Register.tsx
@@ -1,25 +1,44 @@
-import { useState } from "react"
+import axios from "axios";
+import { useState } from "react";
 
 export default function Register() {
+    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const register = () => {
+        if (!username || !email || !password) return;
+        axios.post('http://localhost:3001/register', {
+            username: username.trim(),
+            email: email.trim(), 
+            password: password.trim()
+        })
+        .then(res => console.log(res));
+        setUsername(''); setEmail(''); setPassword('');
+    }
 
     return (
         <div className="input_div">
             <input
                 autoFocus
+                type="text"
+                placeholder="Name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
                 type="email"
-                placeholder="email"
+                placeholder="E-mail"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
             />
             <input
-                placeholder="Your review"
                 type="password"
+                placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
             />
-            <button>Register</button>
+            <button onClick={register}>Register</button>
         </div>
     )
 }
