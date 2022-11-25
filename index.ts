3cc43d14d35174d5576a26df88dd696bc6430bac
Dmitry Kozhevnikov <kdaufa@mail.ru>
2022-11-25 18:55:07 +0500

    add review router

--- index.ts
+++ index.ts
@@ -3,8 +3,11 @@ import mysql from 'mysql';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+import reviewsRouter from './routes/reviews';
+
+
 const app = express();
-const db = mysql.createPool({
+export const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'password',
@@ -18,43 +21,10 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// reviews
-{
-    const table = 'movie_reviews';
+app.use('/reviews', reviewsRouter);
 
-    app.get('/reviews/get', (req, res) => {
-        const sqlGet = `SELECT * FROM ${table}`;
-        db.query(sqlGet, (err, result) => res.send(result));
-    });
 
-    app.post('/reviews/insert', (req, res) => {
-        const { movieName, movieReview } = req.body;
-        const sqlInsert = `INSERT INTO ${table} (movieName, movieReview) VALUES (?, ?)`;
-
-        db.query(sqlInsert, [movieName, movieReview], (err, result) => console.log(result));
-    });
-
-    app.delete('/reviews/delete/:movieName', (req, res) => {
-        const { movieName } = req.params;
-        // ? мы ставим, чтобы в db.query передать этот параметр
-        const sqlDelete = `DELETE FROM ${table} WHERE movieName = ?`;
-        db.query(sqlDelete, movieName, (err, result) => {
-            if (err) console.error(err);
-        })
-        res.send('delete')
-    });
-
-    app.put('/reviews/update', (req, res) => {
-        const { movieName, movieReview } = req.body;
-        const sqlUpdate = `UPDATE ${table} SET movieReview = ? WHERE movieName = ?`;
-
-        db.query(sqlUpdate, [movieReview, movieName], (err, res) => {
-            if (err) console.error(err);
-        });
-    });
-}
-
-// register
+// login & register
 {
     const table = 'users';
     app.post('/register', (req, res) => {
---
+++ routes/reviews.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import { db } from "..";
+
+const reviewsRouter = express.Router();
+const table = 'movie_reviews';
+
+
+reviewsRouter.get('/get', (req, res) => {
+    const sqlGet = `SELECT * FROM ${table}`;
+    db.query(sqlGet, (err, result) => res.send(result));
+});
+
+reviewsRouter.post('/insert', (req, res) => {
+    const { movieName, movieReview } = req.body;
+    const sqlInsert = `INSERT INTO ${table} (movieName, movieReview) VALUES (?, ?)`;
+
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => console.log(result));
+});
+
+reviewsRouter.delete('/delete/:movieName', (req, res) => {
+    const { movieName } = req.params;
+    // ? мы ставим, чтобы в db.query передать этот параметр
+    const sqlDelete = `DELETE FROM ${table} WHERE movieName = ?`;
+    db.query(sqlDelete, movieName, (err, result) => {
+        if (err) console.error(err);
+    })
+    res.send('delete')
+});
+
+reviewsRouter.put('/update', (req, res) => {
+    const { movieName, movieReview } = req.body;
+    const sqlUpdate = `UPDATE ${table} SET movieReview = ? WHERE movieName = ?`;
+
+    db.query(sqlUpdate, [movieReview, movieName], (err, res) => {
+        if (err) console.error(err);
+    });
+});
+
+export default reviewsRouter;
